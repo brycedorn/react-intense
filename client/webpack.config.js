e85bdbe8deae35bc5a5bfcb22ff61cafe9aac64b
Bryce Dorn <brycedorn@gmail.com>
2016-10-31 21:23:56 -0500

    minor changes to config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,10 +3,7 @@ var Path = require("path"),
     webpack = require("webpack"),
     HtmlWebpackPlugin = require("html-webpack-plugin"),
     webpackConfig,
-
-    IS_PRODUCTION = "production" === process.env.NODE_ENV,
-    JSX_WITH_HOT_LOADERS = ["react-hot-loader", "jsx-loader?harmony", "babel-loader?stage=0"],
-    CSS_LOADER = "style-loader!css-loader?root=../",
+    IS_PRODUCTION = false
 
 webpackConfig = module.exports = {
   entry: "./client/scripts/index.js",
@@ -17,9 +14,9 @@ webpackConfig = module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js(x?)$/, loaders: JSX_WITH_HOT_LOADERS },
+      { test: /\.js(x?)$/, loaders: ["jsx-loader?harmony", "babel-loader?stage=0"]},
       { test: /\.jpg$/, loader: "file-loader" },
-      { test: /\.css$/, loader: CSS_LOADER },
+      { test: /\.css$/, loader: "style-loader!css-loader?root=../" },
       { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader'},
     ]
   },
@@ -30,9 +27,3 @@ webpackConfig = module.exports = {
     })
   ]
 };
-
-if (IS_PRODUCTION) {
-  webpackConfig.plugins.push(
-    new webpack.optimize.DedupePlugin()
-  );
-}
