5cced491b08358120f6a8b404fe88ee57cdda237
Bryce Dorn <brycedorn@gmail.com>
2019-02-22 16:37:53 -0600

    anodder fix

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   mode: process.env.NODE_ENV,
   output: {
     path: `${__dirname}/public`,
-    publicPath: devMode ? '/' : 'https://brycedorn.gitlab.io/',
+    publicPath: devMode ? '/' : 'https://brycedorn.gitlab.io/react-intense',
     filename: devMode ? 'bundle.js' : '[name].[hash].js',
   },
   optimization: devMode ? {} : {
